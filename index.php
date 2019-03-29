031051d258896b87eef8d6871e0a2b22203e3d8d
Tomoaki imac5k-1 <tomoaki.nagahara@gmail.com>
2019-03-29 15:46:07 +0900

    New: index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * unit-curl:/index.php
+ *
+ * @creation  2018-07-02
+ * @version   1.0
+ * @package   unit-curl
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+include(__DIR__.'/Curl.class.php');
