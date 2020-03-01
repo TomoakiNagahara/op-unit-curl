d460fd4fb9de1e8355e9042e57928a801aa7d7e4
Tomoaki mbp13r-2015 <tomoaki.nagahara@gmail.com>
2020-03-02 01:03:23 +0900

    New: testcase/get.php

---
+++ testcase/get.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * unit-curl:/testcase/get.php
+ *
+ * @created   2020-03-01
+ * @version   1.0
+ * @package   unit-curl
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+/* @var $curl UNIT\Curl */
+$curl = Unit::Singleton('Curl');
+
+//	...
+$scheme = 'http';
+$host   = $_SERVER['HTTP_HOST'];
+$path   = ConvertURL('app:/api/testcase/');
+$url    = "{$scheme}://{$host}{$path}";
+D($url);
+
+//	...
+$json = $curl->Get($url);
+D($json);
+
+//	...
+$json = json_decode($json, true);
+D($json);
