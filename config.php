30c0c4830cd543c0b60f9035b8d2da4219f052b8
Tomoaki mbp13r-2015 <tomoaki.nagahara@gmail.com>
2020-06-09 16:50:58 +0900

    New: config.php

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+/** op-unit-curl:/config.php
+ *
+ * @created   2020-06-09
+ * @version   1.0
+ * @package   op-unit-curl
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+$ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.4 Safari/605.1.15';
+
+//	...
+$config = [
+	'ua' => $ua,
+];
+
+//	...
+return $config;
